d8f02288a9afd1b87166f0722ab701cf41ce9bcd
LGerMa <lgmm.93@gmail.com>
2017-02-09 08:03:00 -0600

    se añadió modelo 'Products', se crearon las CRUD correspondientes

--- index.js
+++ index.js
@@ -2,6 +2,9 @@
 
 const express = require('express')
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+
+const Product= require('./models/product')
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -10,31 +13,74 @@ app.use(bodyParser.urlencoded({ extended: true}))
 app.use(bodyParser.json())
 
 app.get('/api/product', (req,res) => {
-	res.send(200, {
-		products: []
+	Product.find({}, (err, products) => {
+		if (err) return res.status(500).send({message: `Error al realizar la peticion ${err} `})
+		if (!products) return res.status(404).send({message: `No existen productos`})
+		
+		res.send(200, {products})
 	})
 })
 
 app.get('/api/product/:productId', (req,res) => {
+	let productId = req.params.productId
 
-})
+	Product.findById(productId, (err, product) => {
+		if (err) return res.status(500).send({message: `Error al realizar la peticion ${err} `})
+		if (!product) return res.status(404).send({message: `El producto no existe: `})
 
-app.post('/api/product', (req,res) => {
-	console.log(req.body)
-	//res.send(200, {	message: 'El producto se ha recibido'})
-	res.status(200).send({
-		message: 'El producto se ha recibido'
+		return res.status(200).send({product})
 	})
 })
 
-app.put('/api/product/:productId', (req,res) => {
+app.post('/api/product', (req,res) => {
+	console.log('POST /api/product')
+	console.log(req.body)
 
+	let product = new Product()
+	product.name = req.body.name
+	product.picture = req.body.picture
+	product.price = req.body.price
+	product.category = req.body.category
+	product.description =req.body.description
+
+	product.save((err, productStored) =>{
+		if(err) res.status(500).send({message: `Error al guardar: ${err}`})
+
+		res.status(200).send({product: productStored})
+	})
+
+})
+
+app.put('/api/product/:productId', (req,res) => {
+	let productId = req.params.productId
+	let updateProduct = req.body
+
+	Product.findByIdAndUpdate(productId, updateProduct, (err, productUpdated) =>{
+		if (err) res.status(500).send({message: `Error al actualizar el producti: ${err}`})
+
+		res.status(200).send({product: productUpdated})
+	})
 })
 
 app.delete('/api/product/:productId', (req,res) => {
+	let productId = req.params.productId
 
+	Product.findById(productId, (err, product) => {
+		if (err) res.status(500).send({message: `Error al borrar el producti: ${err}`})
+
+		product.remove(err => {
+			if (err) res.status(500).send({message: `Error al borrar el producti: ${err}`})
+			res.status(200).send({message: `El producto ha sido eliminado`})
+		})
+	})
 })
 
-app.listen(port, () => {
-	console.log(`API REST corriendo en puerto: ${port}`)
+mongoose.connect('mongodb://localhost:27017/shop', (err,res) => {
+	if(err){
+		return console.log(`Error al conectar a la bd... ${err}`)
+	}
+	console.log('Conexion a bd establecida...')
+	app.listen(port, () => {
+		console.log(`API REST corriendo en puerto: ${port}`)
+	})
 })
